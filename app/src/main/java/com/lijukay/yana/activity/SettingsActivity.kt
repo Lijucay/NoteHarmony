dd046693b092be290c46adc7d689ee8c6e7b26ad
Lijukay (Luca) <lico.keins@gmail.com>
2023-12-10 22:46:34 +0100

    SettingsActivity.kt now includes a preference that leads to this projects GitHub page

--- app/src/main/java/com/lijukay/yana/activity/SettingsActivity.kt
+++ app/src/main/java/com/lijukay/yana/activity/SettingsActivity.kt
@@ -1,8 +1,12 @@
-package com.lijukay.yana
+package com.lijukay.yana.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.lijukay.yana.R
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -21,6 +25,17 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val githubPreference: Preference? = findPreference("github_preference")
+
+            githubPreference?.setOnPreferenceClickListener {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(GITHUB_LINK)))
+                true
+            }
         }
+    }
+
+    companion object {
+        private const val GITHUB_LINK: String = "https://www.github.com/Lijukay/YANA"
     }
 }
