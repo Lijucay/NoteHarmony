99d8367d7e3b50c46a4f81195d4ff91b6454ad6a
Lijukay (Luca) <lico.keins@gmail.com>
2023-12-10 22:49:36 +0100

    An adapter to handle note items in an recyclerview was created.

---
+++ app/src/main/java/com/lijukay/yana/adapter/NotesAdapter.kt
@@ -0,0 +1,38 @@
+package com.lijukay.yana.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lijukay.yana.databases.Note
+import com.lijukay.yana.databinding.ItemViewBinding
+
+class NotesAdapter(
+    private var notesList: MutableList<Note> = mutableListOf()
+): RecyclerView.Adapter<NotesAdapter.NotesListViewHolder>() {
+    inner class NotesListViewHolder(val binding: ItemViewBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesListViewHolder {
+        val binding = ItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NotesListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NotesListViewHolder, position: Int) {
+        val currentNoteItem = notesList[position]
+        val noteTitle = currentNoteItem.noteName
+        val noteContent = currentNoteItem.noteContent
+
+        val titleTextView = holder.binding.titleTextView
+        val contentTextView = holder.binding.contentTextView
+
+        titleTextView.text = noteTitle
+        contentTextView.text = noteContent
+    }
+
+    override fun getItemCount(): Int {
+        return notesList.size
+    }
+
+    fun addNote(note: Note) {
+        notesList.add(note)
+    }
+}
