28c1b4d4421ee4584ea1160f7df30accdf712d58
Lijukay (Luca) <lico.keins@gmail.com>
2023-12-10 22:39:40 +0100

    Created Notes Activity

---
+++ app/src/main/java/com/lijukay/yana/activity/NotesActivity.kt
@@ -0,0 +1,157 @@
+package com.lijukay.yana.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.room.Room
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.lijukay.yana.R
+import com.lijukay.yana.adapter.NotesAdapter
+import com.lijukay.yana.databases.Note
+import com.lijukay.yana.databases.NoteDao
+import com.lijukay.yana.databases.YANADatabase
+import com.lijukay.yana.databinding.ActivityNotesBinding
+import com.lijukay.yana.dialogs.CreateNoteDialog
+import com.lijukay.yana.interfaces.OnClickInterface
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class NotesActivity : AppCompatActivity(), OnClickInterface {
+    private lateinit var binding: ActivityNotesBinding
+    private lateinit var noteDao: NoteDao
+    private lateinit var notes: MutableList<Note>
+    private lateinit var collectionName: String
+    private lateinit var notesAdapter: NotesAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        binding = ActivityNotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = intent
+        intent?.let {
+            collectionName = intent.getStringExtra("collection name") ?: getString(R.string.no_collection_name_found)
+        }
+
+        val materialToolbar = binding.materialToolbar
+        val bottomAppBar = binding.bottomAppBar
+        bottomAppBar.menu.findItem(R.id.invisible_menu_item).isVisible = false
+
+        bottomAppBar.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.delete_menu_icon -> {
+                    showConfirmDeletionDialog()
+                    return@setOnMenuItemClickListener true
+                }
+            }
+            return@setOnMenuItemClickListener false
+        }
+
+        materialToolbar.subtitle = collectionName
+        setSupportActionBar(materialToolbar)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val yanaDatabase = Room.databaseBuilder(
+                context = applicationContext,
+                klass = YANADatabase::class.java,
+                name = "yana_database"
+            ).build()
+            noteDao = yanaDatabase.noteDao()
+            notes = noteDao.getNotes(collectionName)
+            notesAdapter = NotesAdapter(notes)
+
+            withContext(context = Dispatchers.Main) {
+                setupUI()
+            }
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun showConfirmDeletionDialog() {
+        MaterialAlertDialogBuilder(this, com.google.android.material.R.style.ThemeOverlay_Material3_MaterialAlertDialog_Centered)
+            .setTitle(getString(R.string.confirm_deletion_title))
+            .setMessage(getString(R.string.confirm_deletion_message, "all notes"))
+            .setPositiveButton(android.R.string.ok) { dialog, _ ->
+                dialog.dismiss()
+                lifecycleScope.launch(Dispatchers.IO) {
+                    val yanaDatabase = Room.databaseBuilder(
+                        context = applicationContext,
+                        klass = YANADatabase::class.java,
+                        name = "yana_database"
+                    ).build()
+                    val noteDao = yanaDatabase.noteDao()
+                    noteDao.deleteAll()
+                    notes.clear()
+                    withContext(Dispatchers.Main) {
+                        notesAdapter.notifyDataSetChanged()
+                        toggleVisibility(notesAdapter.itemCount)
+                    }
+                }
+            }
+            .setNegativeButton(android.R.string.cancel) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setIcon(R.drawable.delete_24px)
+            .show()
+    }
+
+    private fun setupUI() {
+        val notesRecyclerView = binding.noteRecyclerView
+        notesRecyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        notesRecyclerView.adapter = notesAdapter
+
+        toggleVisibility(notesAdapter.itemCount)
+
+        val addFAB = binding.addNoteButton
+        addFAB.setOnClickListener {
+            CreateNoteDialog(
+                context = this@NotesActivity,
+                notesAdapter = notesAdapter,
+                collectionName = collectionName
+            ).show()
+        }
+    }
+
+    fun toggleVisibility(itemCount: Int) {
+        val noNotesInfoTextView = binding.noNotesInfo
+        val notesRecyclerView = binding.noteRecyclerView
+
+        if (itemCount != 0) {
+            noNotesInfoTextView.visibility = View.GONE
+            notesRecyclerView.visibility = View.VISIBLE
+        } else {
+            noNotesInfoTextView.visibility = View.VISIBLE
+            notesRecyclerView.visibility = View.GONE
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.settings_menu_item -> {
+                startActivity(Intent(this@NotesActivity, SettingsActivity::class.java))
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun onClick(position: Int) {
+        Toast.makeText(this@NotesActivity, "Clicked at $position", Toast.LENGTH_SHORT).show()
+    }
+}
